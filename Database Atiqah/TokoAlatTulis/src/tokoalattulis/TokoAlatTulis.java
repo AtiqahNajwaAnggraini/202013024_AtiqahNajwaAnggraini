775b71cd16873a378c1936a1bf1ea1939589b556
AtiqahNajwaAnggraini <80561542+AtiqahNajwaAnggraini@users.noreply.github.com>
2021-07-11 21:59:11 +0700

    Add files via upload

---
+++ Database Atiqah/TokoAlatTulis/src/tokoalattulis/TokoAlatTulis.java
@@ -0,0 +1,23 @@
+package tokoalattulis;
+
+import java.sql.Connection; //
+import java.sql.DriverManager;//digunakan untuk memanggail driver JDBC ke memori dan juga untuk membuka koneksi ke sumber data.
+import java.sql.Statement;
+ 
+public class TokoAlatTulis {
+    public static Connection con;
+    public static Statement stm;
+    public static void main(String args[]){
+        try {
+            String url ="jdbc:mysql://localhost/toko_alat_tulis";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con =DriverManager.getConnection(url,user,pass);
+            stm = con.createStatement();
+            System.out.println("koneksi berhasil;");
+        } catch (Exception e) {
+            System.err.println("koneksi gagal" +e.getMessage());
+        }
+    }
+}
